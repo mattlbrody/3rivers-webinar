1fa5e0def6d11db5647bf5f223e885b3653d9f5a
mattlbrody <mattlbrody@gmail.com>
2020-08-28 08:46:39 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// var colors = ["red", "green", "blue"]
+// colors.forEach(function(color) {
+// 	document.write(color);
+// });
+
+// var numbers = [1, 3, 5, 7, 9], 
+// 	sum = 0
+
+// function addNumbers(numb) {
+// 	sum += numb
+// }
+
+// numbers.forEach(addNumbers)
+// console.log(sum + 1)
+
+{
+  var user = "Ryan";
+}
+console.log(user); // Ryan
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log(8/2*(2+2))
